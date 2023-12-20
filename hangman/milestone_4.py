6a9c0a42242b12e5de237b0d5511cfa326165369
Sean Dowden <you@example.com>
2023-12-20 15:42:21 +0000

    created reactive lives counter to game, decreasing with incorrect guesses

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -13,19 +13,27 @@ class hangman:
         self.list_of_guesses = []
 
 
-    def check_guess(word_guessed):
+    def check_guess(guess):
 
 
         word = random.choice(word_list)
 ## print(word)
-
+        num_lives = 5
         guess = input("Please enter a single letter: ")
         guess = guess.lower()
+        word_guessed = ['_' for _ in word]
+        num_letters = len(set(word))
 
         if guess in word:
             print(f"Good guess! {guess} is in the word.")
+            for i in range(len(word)):
+                if word[i] == guess:
+                    word_guessed[i] = guess
+            num_letters -= 1
         else:
+            num_lives -= 1
             print(f"Sorry, {guess} is not in the word. Try again.")
+            print(f"You have {num_lives} lives left.")
 
     def ask_for_input(check_guess):
         list_of_guesses = []
@@ -38,8 +46,8 @@ class hangman:
             else:
                 check_guess(guess)
                 list_of_guesses.append(guess)
-                return
+                
 hangman = hangman(word_list)
 
 # Call the ask_for_input method
-hangman.ask_for_input()
+hangman.check_guess()
